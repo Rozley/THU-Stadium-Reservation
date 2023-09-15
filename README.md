f47076bb6ad6b2df6041abe6c8a98ba368f51bad
Rozley <xiaohan.li2@dxc.com>
2023-09-15 11:16:08 +0800

    删除json

---
+++ README.md
@@ -0,0 +1,25 @@
+# THU-Badminton-Reservation
+
+某大学体育场地预定🏸
+
+## accounts.json
+```
+"acc0": {
+    "user_id": "your_student_id",
+    "user_password": "your_password",
+    "tasks":[
+      {
+        "gym": "综合体育馆",
+        "sport": "羽毛球",
+        "date": "2023-09-18",
+        "time": "20:00-22:00"
+      },
+      {
+        "gym": "西体育馆",
+        "sport": "羽毛球",
+        "date": "2023-09-18",
+        "time": "20:00-22:00"
+      }
+    ]
+  }
+```
---
+++ badminton.py
@@ -0,0 +1,108 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import json
+
+# 登陆页
+login_url = "https://50.tsinghua.edu.cn/dl.jsp"
+# 抢票目标页
+target_url = "https://50.tsinghua.edu.cn/gymbook/gymBookAction.do?ms=viewGymBook"
+# 账户信息json
+account_path = "./accounts.json"
+# 不同体育馆的id，前面是体育馆id，后面是羽毛球项目id
+gym_ids = {
+    "综合体育馆羽毛球": ["4797914", "4797899"],
+    "综合体育馆篮球": ["4797914", "4797898"],
+    "西体育馆羽毛球": ["4836273", "4836196"],
+    "西体育馆台球": ["4836273", "14567218"],
+    "西体育馆篮球": ["4836273", "19071481"],
+    "气膜体育馆羽毛球": ["3998000", "4045681"],
+    "气膜体育馆乒乓球": ["3998000", "4037036"],
+}
+# 是否需要刷新等待
+need_refresh = True
+
+
+class Badminton:
+    def __init__(self):
+        # 默认Chrome浏览器
+        self.driver = webdriver.Chrome()
+        # 用户账户信息
+        self.account_info = {}
+        # 隐式等待，若组件未出现则进入等待，等待期间将持续寻找组件
+        self.driver.implicitly_wait(1)
+
+    def parse_account(self):
+        with open(account_path, 'r') as f:
+            reader = json.load(f)
+            self.account_info = reader['acc0']
+            print(self.account_info)
+
+    def login(self):
+        # 打开登录页
+        self.driver.get("https://50.tsinghua.edu.cn/dl.jsp")
+        username = self.driver.find_element(By.ID, "login_username")
+        password = self.driver.find_element(By.ID, "login_password")
+        username.send_keys(self.account_info['user_id'])
+        password.send_keys(self.account_info['user_password'])
+        login_btn = self.driver.find_element(By.XPATH, "//input[@alt='Login']")
+        login_btn.click()
+        # 打开目标页面
+        self.driver.find_element(By.XPATH, '//*[@id="nav-main"]/ul/li[3]/a').click()
+        self.driver.find_element(By.XPATH, '//*[@id="bookInfo"]/div/div[2]/a').click()
+        for task in self.account_info['tasks']:
+            self.driver.get(target_url +
+                            "&gymnasium_id=" + gym_ids[task['gym']+task['sport']][0] +
+                            "&item_id=" + gym_ids[task['gym']+task['sport']][1] +
+                            "&time_date=" + task['date'] +
+                            "&userType=")
+
+            WebDriverWait(self.driver, 10, 0.1).until(EC.element_to_be_clickable((By.XPATH, "//*[text()='同意']")))
+            agree_btn = self.driver.find_element(By.XPATH, "//*[text()='同意']")
+            agree_btn.click()
+
+            while need_refresh:
+                try:
+                    self.driver.implicitly_wait(0)
+                    iframe = self.driver.find_element(By.ID, "overlayView")
+                    self.driver.implicitly_wait(10)
+                    break
+                except Exception as e:
+                    self.driver.refresh()
+                    time.sleep(0.2)
+
+            iframe = self.driver.find_element(By.ID, "overlayView")
+            self.driver.switch_to.frame(iframe)
+
+            non_ava_fields = self.driver.find_elements(By.XPATH, "//td[@time_session=\'" + task['time'] +
+                                                   "\' and @lock='true']")
+            all_fields = self.driver.find_elements(By.XPATH, "//td[@time_session=\'" + task['time'] + "\']")
+            ava_fields = list(set(all_fields).difference(set(non_ava_fields)))
+            ava_fields[-1].click()
+
+            self.driver.switch_to.default_content()
+
+            odd_btn = self.driver.find_element(By.XPATH, "//span[@onclick='saveOdder()']")
+            odd_btn.click()
+
+            time.sleep(10)
+
+    def choose_field(self):
+        self.parse_account()
+        self.login()
+
+    def finish(self):
+        self.driver.quit()
+
+
+if __name__ == '__main__':
+    # initialize badminton class
+    bm = Badminton()
+    try:
+        bm.choose_field()
+
+    except Exception as e:
+        print(e)
+        bm.finish()
